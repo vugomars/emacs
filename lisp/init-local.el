83c6fe0837cf41940bb06c19df1494f8e5dca3d5
vugomars <vugomars@gmail.com>
2022-02-27 22:23:56 +0700

    Fix Keybinding

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -36,15 +36,70 @@
     :keymaps '(normal insert visual emacs)
     :prefix "SPC"
     :global-prefix "C-SPC")
-
   (dqv/leader-keys
     "t"  '(:ignore t :which-key "toggles")
     "tt" '(counsel-load-theme :which-key "choose theme")
-    "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/config.org")))))
+    "fde" '(lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/config.org")))
+
+    ;; Dired
+    "ff" '(find-file :which-key "Find Files")
+    "fr" '(recentf-open-files :which-key "Recentf Open Files")
+
+    ;; Vterm
+    "ot" '(vterm-other-window :which-key "Vterm Other Windown")
+    "oT" '(vterm :which-key "Vterm")
+
+    ;; Treemacs
+    "m"  '(:ignore t :which-key "Treemacs")
+    "mt" '(treemacs :which-key "Treemacs")
+    "mdf" '(treemacs-delete-file :which-key "Delete Files")
+    "mdp" '(treemacs-remove-project-from-workspace :which-key "Delete Projects")
+    "mcd" '(treemacs-create-dir :which-key "Create Directory")
+    "mcf" '(treemacs-create-file :which-key "Create File")
+    "ma" '(treemacs-add-project-to-workspace :which-key "Add Projects")
+    "mc" '(treemacs-create-workspace :which-key "Create Workspace")
+    "ms" '(treemacs-switch-workspace :which-key "Switch Workspace")
+    "md" '(treemacs-remove-workspace :which-key "Remove Workspace")
+    "mf" '(treemacs-rename-workspace :which-key "Rename Workspace")
+
+    ;; Buffer
+    "j" '(switch-to-buffer :which-key "Switch Buffer")
+    "bk" '(kill-this-buffer :which-key "Kill This Buffer")
+    ))
 
 
 (when (maybe-require-package 'evil-collection)
   :config
   (evil-collection-init))
+
+
+;; Native Compilation
+;; Silence compiler warnings as they can be pretty disruptive
+(setq native-comp-async-report-warnings-errors nil)
+
+;; Set the right directory to store the native comp cache
+(add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))
+
+;; Startup Performance
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "*** Emacs loaded in %s seconds with %d garbage collections."
+                     (emacs-init-time "%.2f")
+                     gcs-done)))
+
+;; Keep Folders Clean
+(when (maybe-require-package 'no-littering))
+
+;; no-littering doesn't set this by default so we must place
+;; auto save files in the same path as it uses for sessions
+(setq auto-save-file-name-transforms
+      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
+(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (provide 'init-local)
